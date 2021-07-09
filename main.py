ccb9cd8393ef7859879c4fea54c81bbec574db9e
Jenna Rowan <jennagirl001@gmail.com>
2021-07-09 17:30:33 -0400

    Added handling bad user input, case sensitivity

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import json
 # Loads in the definitions file as a dict
 data = json.load(open("data.json","r"))
 
+def print_definition(user_word):
+
+    # Personal terminal cleanliness preference
+    print("")
+
+    # Loops through all possible definitions of the word and prints them
+    for definition in data[user_word]:
+
+        print(definition)
+
 # Grabs the word to be looked up
-userWord = input("\nPlease enter a word to be defined:\n")
+user_word = input("\nPlease enter a word to be defined:\n")
 
-print(f"\nThe defintion(s) of {userWord}:\n")
+try:
+    print_definition(user_word.lower())
 
-# Loops through all possible definitions of the word and prints them
-for definition in data[userWord]:
+except KeyError:
+    print("I'm sorry but that word is not in our dictionary.  Please try again.")
 
-    print(definition)
-
+# Personal terminal cleanliness preference
 print("")
